2da7d067131302cf484e91640e9fb7e2b6df6f59
hadeelfleifel <hadeelfleifel@gmail.com>
2019-09-01 10:55:34 +0300

    php

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+
+
+<!-- /* Question 1 : Write a PHP script to display names and the salaries amounts,
+ within a table. ou should use `echo ` to generate your table. */
+//////////////////////// Your Code Here ///////////////////// -->
+
 <?php
+$counter=0;
+$names=array("hadeel","raghade");
+$salaries_amounts=array("first","sec");
 
-/* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
-//////////////////////// Your Code Here /////////////////////
+while($counter<2){
+echo " $names[$counter].$salaries_amounts[$counter].<br/>";
+$counter++;
+}
 
 
+?>
+<br/>
 
 
-
-/*Question 2 : Write a PHP script which displays the capital and country name from the below array $c. as unordered list after sorting the array list by countries names  */
-//////////////////////// Your Code Here /////////////////////
-
+<!-- /*Question 2 : Write a PHP script which displays the capital and country name from the below array 
+$c. as unordered list after sorting the array list by countries names  */
+//////////////////////// Your Code Here ///////////////////// -->
+<?php
 $countries = array( "Italy"=>"Rome",
               "Luxembourg"=>"Luxembourg",
                "Belgium"=> "Brussels",
@@ -18,7 +45,18 @@ $countries = array( "Italy"=>"Rome",
                "France" => "Paris",
                "Slovakia"=>"Bratislava" ) ;
 
-/*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
+            //    print_r($countries);
+            //    foreach ($countries as $key => &$val) {}
+                
+            //     var_dump($arr1);
+
+                while (list($key, $value) = each($countries)) { 
+                    echo "$key => $value \r\n"; 
+                }
+                ?>
+<br/>
+
+<!-- /*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
 
 *
 * *
@@ -31,11 +69,52 @@ $countries = array( "Italy"=>"Rome",
 * *
 *
 
-*/
+*/ -->
+<?php
+$stars="";
+$star= "*";
+for($i=0;$i<5;$i++){
+    for($y=0;$y<5;$y++){
+        $stars= $stars.$star;
+        echo $stars ."\t\n";
+    }
+    echo $stars;
+    for($z=5;$z>1;$z--){
+        $stars= $stars-$star;
+        echo $stars."\t\n" ;
 
-/*Question 4 : Write a PHP script to calculate the difference between two dates.
+    }
+}
+
+?>
+<br/>
+
+<!-- /*Question 4 : Write a PHP script to calculate the difference between two dates.
 Input : 1981-11-04, 2013-09-04
 Output : 31 years, 10 months, 11 days
+*/ -->
+
+<?php
+$date1 =date("04,11,1981");             
+
+$date2 =date("04,9,2013");
+
+$diff =abs($date2-$date1);
+echo $diff;
 
 
-/*Question 5 : Create a simple HTML form that accept the user name after submitting display the name using PHP echo statement under the form. */
+?>
+<br/>
+
+<!-- 
+/*Question 5 : Create a simple HTML form that accept the user name after submitting display
+ the name using PHP echo statement under the form. */ -->
+<?php
+$username="Hadeel Fleifel";
+
+echo $username;
+
+?>
+
+ </body>
+</html>
